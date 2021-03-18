8b8f0c7a48c08eb0587de753364a9bb8a991055e
dpeger <dpeger@users.noreply.github.com>
2021-03-18 14:28:49 +0100

    Refactored to support Thymeleaf and JSP templates

--- 10 - AdminToolWeb/src/main/java/com/zetcode/Application.java
+++ 10 - AdminToolWeb/src/main/java/com/zetcode/Application.java
@@ -4,9 +4,16 @@ package com.zetcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import javax.annotation.Resource;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
@@ -15,5 +22,54 @@ public class Application extends SpringBootServletInitializer {
         SpringApplication.run(Application.class, args);
     }
 
+    @Resource
+    protected ApplicationContext applicationContext;
 
+    @Resource
+    protected SpringTemplateEngine springTemplateEngine;
+
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(){
+        final ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setViewNames(new String[] {"thyme/*"});
+        viewResolver.setExcludedViewNames(new String[] {"jsp/*"});
+        viewResolver.setTemplateEngine(springTemplateEngine);
+        viewResolver.setCharacterEncoding("UTF-8");
+        return viewResolver;
+    }
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver(){
+        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/templates/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewNames("jsp/*");
+        return viewResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver thymeleafTemplateResolver(){
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+        templateResolver.setOrder(0);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver jspTemplateResolver(){
+        final SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/templates/");
+        templateResolver.setSuffix(".jsp");
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(false);
+        templateResolver.setOrder(1);
+        templateResolver.setCharacterEncoding("UTF-8");
+        return templateResolver;
+    }
 }
